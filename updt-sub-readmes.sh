4c96d9f2121b29461fee2f60557bc85e5af58649
Ernani Kern <ernani.kern@gmail.com>
2025-09-12 09:19:43 -0300

    updt-sub-readmes.sh

---
+++ updt-sub-readmes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Verifica se um parâmetro foi fornecido e se o branch existe <<<<<
+if [ -z "$1" ]; then
+  echo "Por favor, forneça um branch."
+  exit 1
+fi
+
+# verifica se o branch existe
+if git show-ref --verify --quiet refs/heads/"$1"; then
+    echo "Branch existe"
+else
+    echo "Branch não existe"
+    exit 1
+fi
+
+# Armazena o parâmetro em uma variável (opcional)
+branch=$1
+
+# Exibe o parâmetro
+echo "O branch fornecido foi: $branch"
+
+# Faz switch para o branch
+echo "git switch $branch..."
+git switch "$branch"
+
+# Busca o README de main: 
+#echo "git restore --source main README.md..."
+#git restore --source main README.md
+
+# Abre o meld para edição do README do branch
+echo "Abrindo meld para edição do README de $branch..."
+git show main:README.md > /tmp/readme
+meld /tmp/readme "$branch"/README.md
+
+# Ao fechar o meld, faz novo restore do README do main
+# echo "git restore README.md..."
+# git restore README.md
+
+# Faz commit no README do branch
+echo "git add $branch/README.md..."
+echo "git commit -m "$branch"/README.md..."
+git add "$branch"/README.md
+git commit -m "updt README do $branch"
+
